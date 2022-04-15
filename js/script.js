09714ab1d82ab0901275fc5dbccf0fc931dcc660
NikitaShevchik <shevchik.nikita@gmail.com>
2022-04-15 15:57:35 +0300

    adaptive done 15.04

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const menuBurgerIcon = document.querySelector('.header__icon');
 const menuBurgerDisplay = document.querySelector('.menu');
+const headerLogo = document.querySelector('.header')
 
 menuBurgerIcon.addEventListener('click', openMenu);
 
@@ -9,4 +10,5 @@ function openMenu(){
     menuBurgerDisplay.classList.toggle('_active');
     menuBurgerIcon.classList.toggle('_cross');
     document.body.classList.toggle('_block');
+    headerLogo.classList.toggle('_background')
 }
